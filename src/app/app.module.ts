497382f39acde304fe98783ba8a4d2cb52d66d94
leticiaslucena <leticiasantos.lucena@homail.com>
2023-05-19 15:03:12 -0300

    angular blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componente/menu/menu.component';
+import { MenuTituloComponent } from './componente/menu-titulo/menu-titulo.component';
+import { BigCardComponent } from './componente/big-card/big-card.component';
+import { SmallCardComponent } from './componente/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuTituloComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componente/menu-titulo/menu-titulo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-titulo',
+  templateUrl: './menu-titulo.component.html',
+  styleUrls: ['./menu-titulo.component.css']
+})
+export class MenuTituloComponent {
+
+}
