82b8fb87a7cf754638128298b5fcb19f20f7b1e7
leticiaslucena <leticiasantos.lucena@homail.com>
2023-05-23 18:04:32 -0300

    atualização nos cards

--- src/app/componente/big-card/big-card.component.ts
+++ src/app/componente/big-card/big-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit  {
+  @Input()  
+  fotoCover: string =""
+  @Input() 
+  cardTitulo: string =""
+  @Input() 
+  cardDescricao: string =""
 
+  constructor() {}
+
+  ngOnInit (): void{
+
+  }
 }
--- src/app/componente/menu-titulo/menu-titulo.component.ts
+++ src/app/componente/menu-titulo/menu-titulo.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-titulo',
   templateUrl: './menu-titulo.component.html',
-  styleUrls: ['./menu-titulo.component.css']
+  styleUrls: ['./menu-titulo.component.css', './menu-titulo.component.responsive.css']
 })
 export class MenuTituloComponent {
 
